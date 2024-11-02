2d266d19f69f58428907777544b65c73392048dd
THGeek <alex24ht@hotmail>
2024-11-02 19:39:59 +0800

    feat: add useState and set the item active if clicked

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,28 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // Hook
+  // Tell React this component can data or state that changes over time
+  const [selectedIndex, setSlectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSlectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
